fb6b914be4d949f5b80bd5cd4453a37bcfa4eafb
Emilie Thoreson <thoresonemilie@gmail.com>
2018-12-16 11:30:44 -0700

    pause time calculate

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -9,40 +9,70 @@ export default class VideoPlayer extends React.Component {
     this.pauseClicked = this.pauseClicked.bind(this)
     this.videoFinished = this.videoFinished.bind(this)
     this.stateChanged = this.stateChanged.bind(this)
-    this.state = {}
+    this.state = {sourceFeed: []}
   }
 
   componentDidMount() {
     var self = this;
     var youtube = require('youtube-random-video');
     youtube.getRandomVid('AIzaSyALQDoCiusD0Poqe2mDgGo78zoQy31U2N0', function(err, data) {
-      console.log('DATA ID:', data.id.videoId);
       var videoId = data.id.videoId;
       self.setState({videoSource: videoId});
       return;
     })
   }
 
-  playClicked() {
-
+  playClicked(event) {
+    var currentTime = Math.round(event.target.getCurrentTime() * 100) / 100;
+    var statusFeed = this.state.sourceFeed;
+    if (this.state.pausedAt) {
+      var d = new Date();
+      var t = d.getTime();
+      let timeUnit;
+      var timePaused = (t - this.state.pausedAt) / 1000
+      if (timePaused < 60) {
+        timePaused = Math.round(timePaused * 100) / 100;
+        timeUnit = " seconds"
+      } else if (timePaused > 3600) {
+        var hours = Math.floor(timePaused / 3600);
+        //var mins = Math.floor(timePaused - hours )
+        timeUnit = " hours"
+      } else {
+        var mins = Math.floor(timePaused / 60);
+        var secs = Math.round(timePaused - mins * 60);
+        timePaused = mins + ':' + secs;
+        timeUnit = " minutes"
+      }
+      var resumedMessage = 'unpaused after ' + timePaused + timeUnit;
+      statusFeed.push({time: currentTime, message: resumedMessage})
+    } else {
+      statusFeed.push({time: currentTime, message: 'played'})
+    }
+    this.setState({sourceFeed: statusFeed})
   }
 
-  pauseClicked() {
+  pauseClicked(event) {
+    var currentTime = Math.round(event.target.getCurrentTime() * 100) / 100;
+    var statusFeed = this.state.sourceFeed;
+    statusFeed.push({time: currentTime, message: 'paused'})
+    var d = new Date();
+    var t = d.getTime();
+    this.setState({sourceFeed: statusFeed, pausedAt: t})
 
   }
 
   stateChanged(event) {
     var videoStatus = "";
-    if (event.data == 1) {
-      videoStatus += "Playing"
+    if (event.data == 0) {
+      videoStatus += "Completed"
+    } else if (event.data == 1) {
+        videoStatus += "Playing"
     } else if (event.data == 2) {
       videoStatus += "Paused"
     } else if (event.data == 3) {
       videoStatus += "Buffering"
     } else if (event.data == 5) {
       videoStatus += "Loaded"
-    } else if (event.data == 0) {
-      videoStatus += "Completed"
     }
     this.setState({videoStatus: videoStatus})
   }
@@ -52,7 +82,7 @@ export default class VideoPlayer extends React.Component {
   }
 
   render() {
-    var src = this.state.videoSource;
+    var src = this.state.videoSource ? this.state.videoSource : "";
     var currentVideoStatus = this.state.videoStatus ? "Video Status: " + this.state.videoStatus : null;
 
     var opts = {
@@ -62,6 +92,10 @@ export default class VideoPlayer extends React.Component {
         autoplay: 0
       }
     };
+
+    var statusFeed = this.state.sourceFeed.map(function(update, index) {
+      return <div key={index}>{update.time} - {update.message}</div>
+    });
 
     return (
       <div className="videoPlayerView">
@@ -76,6 +110,9 @@ export default class VideoPlayer extends React.Component {
           onStateChange={this.stateChanged}
 
         />
+        <div className="statusFeed">
+          {statusFeed}
+        </div>
       </div>
     );
   }
